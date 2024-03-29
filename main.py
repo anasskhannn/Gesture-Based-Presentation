ca660fa9524cbd5495c1a8e333b7e329d6a53cf6
Mohd Anas <anascollege2129@eng.rizvi.edu.in>
2024-03-29 15:59:30 +0530

    Getting List of Images of Presentation

--- main.py
+++ main.py
@@ -1,17 +1,25 @@
 # starting with the web cam
+import os
+
 import cv2
 
 # Variables
+# Camera Width & Height
 width,height= 1200, 720
+# Folder Path
+folderpath="img"
+
 
 # Camera setup
-
 cap= cv2.VideoCapture(0)
 cap.set(3,width)
 cap.set(4,height)
 
+imgPath=os.listdir(folderpath)
+print(imgPath)
 
 while True:
+    # Import Images
     sucess,img=cap.read()
     cv2.imshow("Image", img)
     key= cv2.waitKey(1)
