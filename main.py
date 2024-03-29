98a12e23761e4a60d39bfc01372641b627b6b2fb
Mohd Anas <anascollege2129@eng.rizvi.edu.in>
2024-03-29 16:44:01 +0530

    Getting the image and also Resize the Slide Images in Proper Format to stay inside display

--- main.py
+++ main.py
@@ -5,25 +5,34 @@ import cv2
 
 # Variables
 # Camera Width & Height
-width,height= 1200, 720
+width,height= 720, 480
 # Folder Path
 folderpath="img"
 
 
 # Camera setup
 cap= cv2.VideoCapture(0)
-cap.set(3,width)
-cap.set(4,height)
+cap.set(3, width)
+cap.set(4, height)
 
 # getting Presentation Images
 # We need to sort this to 10 will be at last not after 1
 imgPath=sorted(os.listdir(folderpath),key=len)
-print(imgPath)
+# print(imgPath)
+
+# Number of Image to show
+imgNum= 3
 
 while True:
     # Import Images
     sucess,img=cap.read()
-    cv2.imshow("Image", img)
+
+    fullimg=os.path.join(folderpath,imgPath[imgNum])
+    CurrentImg=cv2.imread(fullimg)
+    CurrentImgResize=cv2.resize(CurrentImg,(720,480))
+
+    cv2.imshow("WEBCAM", img)
+    cv2.imshow("Slides", CurrentImgResize)
     key= cv2.waitKey(1)
     # Adding if to close webcam and break the loop by pressing "Q"
     if key == ord("q"):
